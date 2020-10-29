6e8951754a7555f2a82dd205fef03e9f745473d8
Michael Leonhard <michael@leonhardllc.com>
2020-10-29 00:50:47 -0700

    Don't try to add rustfmt or clippy components.  Rely on cargo downloading them automatically.

--- check.sh
+++ check.sh
@@ -6,9 +6,7 @@ check() {
   time cargo check --verbose
   time cargo build --verbose
   time cargo test --verbose
-  time rustup component add rustfmt
   time cargo fmt --all -- --check
-  time rustup component add clippy
   time cargo clippy -- -D warnings
   time cargo publish --dry-run "$@"
   echo "$0 finished"
