869ba4a0ac965f4759cbefc056d25f29e2af36c1
Michael Leonhard <michael@leonhardllc.com>
2020-12-07 21:43:09 -0800

    Test ReadWriteTake

--- fixed-buffer/src/read_write_take.rs
+++ fixed-buffer/src/read_write_take.rs
@@ -1,6 +1,5 @@
 #![forbid(unsafe_code)]
 
-// TODO(mleonhard) Test.
 /// Wraps a struct that implements
 /// [`Read`](https://doc.rust-lang.org/stable/std/io/trait.Read.html)+[`Write`](https://doc.rust-lang.org/stable/std/io/trait.Write.html).
 /// Passes through reads and writes to the struct.
@@ -50,5 +49,125 @@ impl<'a, RW: std::io::Read + std::io::Write> std::io::Write for ReadWriteTake<'a
 
     fn flush(&mut self) -> Result<(), std::io::Error> {
         self.read_writer.flush()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+
+    #[test]
+    fn read_error() {
+        let mut read_writer = FakeReadWriter::new(vec![Err(err1()), Ok(2), Ok(0)]);
+        let mut take = ReadWriteTake::new(&mut read_writer, 3);
+        let mut buf = [b'.'; 4];
+        assert_eq!(
+            "err1",
+            std::io::Read::read(&mut take, &mut buf)
+                .unwrap_err()
+                .to_string()
+        );
+        assert_eq!(2, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("ab..", escape_ascii(&buf));
+        assert_eq!(0, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("ab..", escape_ascii(&buf));
+    }
+
+    #[test]
+    fn empty() {
+        let mut read_writer = FakeReadWriter::new(vec![Ok(0)]);
+        let mut take = ReadWriteTake::new(&mut read_writer, 3);
+        let mut buf = [b'.'; 4];
+        assert_eq!(0, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("....", escape_ascii(&buf));
+    }
+
+    #[test]
+    fn doesnt_read_when_zero() {
+        let mut read_writer = FakeReadWriter::empty();
+        let mut take = ReadWriteTake::new(&mut read_writer, 0);
+        let mut buf = [b'.'; 4];
+        assert_eq!(0, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("....", escape_ascii(&buf));
+    }
+
+    #[test]
+    fn fewer_than_len() {
+        let mut read_writer = FakeReadWriter::new(vec![Ok(2), Ok(0)]);
+        let mut take = ReadWriteTake::new(&mut read_writer, 3);
+        let mut buf = [b'.'; 4];
+        assert_eq!(2, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("ab..", escape_ascii(&buf));
+        assert_eq!(0, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("ab..", escape_ascii(&buf));
+    }
+
+    #[test]
+    fn fewer_than_len_in_multiple_reads() {
+        let mut read_writer = FakeReadWriter::new(vec![Ok(2), Ok(2), Ok(0)]);
+        let mut take = ReadWriteTake::new(&mut read_writer, 5);
+        let mut buf = [b'.'; 4];
+        assert_eq!(2, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("ab..", escape_ascii(&buf));
+        assert_eq!(2, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("cd..", escape_ascii(&buf));
+        assert_eq!(0, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("cd..", escape_ascii(&buf));
+    }
+
+    #[test]
+    fn exactly_len() {
+        let mut read_writer = FakeReadWriter::new(vec![Ok(3), Ok(0)]);
+        let mut take = ReadWriteTake::new(&mut read_writer, 3);
+        let mut buf = [b'.'; 4];
+        assert_eq!(3, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("abc.", escape_ascii(&buf));
+        assert_eq!(0, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("abc.", escape_ascii(&buf));
+    }
+
+    #[test]
+    fn exactly_len_in_multiple_reads() {
+        let mut read_writer = FakeReadWriter::new(vec![Ok(2), Ok(1), Ok(0)]);
+        let mut take = ReadWriteTake::new(&mut read_writer, 3);
+        let mut buf = [b'.'; 4];
+        assert_eq!(2, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("ab..", escape_ascii(&buf));
+        assert_eq!(1, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("cb..", escape_ascii(&buf));
+        assert_eq!(0, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("cb..", escape_ascii(&buf));
+    }
+
+    #[test]
+    fn doesnt_call_read_after_len_reached() {
+        let mut read_writer = FakeReadWriter::new(vec![Ok(3)]);
+        let mut take = ReadWriteTake::new(&mut read_writer, 3);
+        let mut buf = [b'.'; 4];
+        assert_eq!(3, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("abc.", escape_ascii(&buf));
+        assert_eq!(0, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("abc.", escape_ascii(&buf));
+    }
+
+    #[test]
+    fn doesnt_call_read_after_len_reached_in_multiple_reads() {
+        let mut read_writer = FakeReadWriter::new(vec![Ok(2), Ok(1)]);
+        let mut take = ReadWriteTake::new(&mut read_writer, 3);
+        let mut buf = [b'.'; 4];
+        assert_eq!(2, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("ab..", escape_ascii(&buf));
+        assert_eq!(1, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("cb..", escape_ascii(&buf));
+        assert_eq!(0, std::io::Read::read(&mut take, &mut buf).unwrap());
+        assert_eq!("cb..", escape_ascii(&buf));
+    }
+
+    #[test]
+    fn passes_writes_through() {
+        let mut read_writer = FakeReadWriter::new(vec![Ok(3)]);
+        let mut take = ReadWriteTake::new(&mut read_writer, 2);
+        assert_eq!(3, std::io::Write::write(&mut take, b"abc").unwrap());
+        assert!(read_writer.is_empty());
     }
 }
