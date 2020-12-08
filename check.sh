c7d776521f10bb0a82eb02799b553c911fdad614
Michael Leonhard <michael@leonhardllc.com>
2020-12-07 20:02:52 -0800

    shellfmt

--- check.sh
+++ check.sh
@@ -5,7 +5,10 @@
 
 check_cargo_readme() {
   cargo readme >Readme.md.tmp
-  diff Readme.md Readme.md.tmp || (echo "Readme.md is stale" >&2; exit 1)
+  diff Readme.md Readme.md.tmp || (
+    echo "Readme.md is stale" >&2
+    exit 1
+  )
   rm -f Readme.md.tmp
   git rm -f --ignore-unmatch Readme.md.tmp
 }
