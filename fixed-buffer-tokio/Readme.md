47e39d7986204bb675450a23a1d8aaa851c173a7
Michael Leonhard <michael@leonhardllc.com>
2020-12-08 22:51:45 -0800

    Release fixed-buffer-tokio v0.1.1

--- fixed-buffer-tokio/Readme.md
+++ fixed-buffer-tokio/Readme.md
@@ -41,6 +41,7 @@ For a complete example, see
 1. Run `../release.sh`
 
 ## Changelog
+- v0.1.1 - Add badges to readme
 - v0.1.0 - First published version
 
 
--- fixed-buffer-tokio/src/lib.rs
+++ fixed-buffer-tokio/src/lib.rs
@@ -39,6 +39,7 @@
 //! 1. Run `../release.sh`
 //!
 //! # Changelog
+//! - v0.1.1 - Add badges to readme
 //! - v0.1.0 - First published version
 //!
 #![forbid(unsafe_code)]
